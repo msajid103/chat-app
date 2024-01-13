cf8c7740ec9b839da4078194aee0f2197614880f
msajid103 <msajidnaizi@gmail.com>
2024-01-13 21:43:04 +0500

    the chat app now able to push and pull encrpted messages on database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -9,17 +9,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Person(db.Model):
-    ph_no = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), unique=True, nullable=False)
+    ph_no = db.Column(db.String(11), primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
     messages_sent = db.relationship('Message', backref='sender', lazy=True, foreign_keys='Message.sender_id')
     messages_received = db.relationship('Message', backref='receiver', lazy=True, foreign_keys='Message.receiver_id')
 
 class Message(db.Model):   
     id = db.Column(db.Integer, primary_key=True) 
     content = db.Column(db.Text, nullable=False)
-    sender_id = db.Column(db.Integer, db.ForeignKey('person.ph_no'), nullable=False)
-    receiver_id = db.Column(db.Integer, db.ForeignKey('person.ph_no'), nullable=False)
+    sender_id = db.Column(db.String(11), db.ForeignKey('person.ph_no'), nullable=False)
+    receiver_id = db.Column(db.String(11), db.ForeignKey('person.ph_no'), nullable=False)
 
 
 def encrypt(message):
@@ -37,24 +37,35 @@ def decrypt(encrypted_message):
         index = key.index(char)
         decrypted_message += key[(index - 3) % len(key)]
     return decrypted_message
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
+#     db.session.query(Message).delete()
+#     db.session.commit()
+    
+
 @app.route('/')
 def index():
     return render_template('login_signup.html')
-
+users = None
+phn = None
 @app.route('/login', methods=['POST'])
 def login():
+    global users
+    global phn
     phone_number = request.form.get('loginPhoneNumber')
     password = request.form.get('loginPassword')
     user = Person.query.filter_by(ph_no=phone_number, password=password).first()
-    
-
     if user:
-        session['user_id'] = phone_number
-        users = Person.query.filter(Person.ph_no != phone_number).all()
-        # user_names = [person.name for person in users]
-        return render_template('index.html', user_names = users, name= Person.query.get(phone_number).name)
+        phn = phone_number
+        users = Person.query.filter(Person.ph_no != phone_number).all()  
+        messages = Message.query.filter(
+        ((Message.sender_id == phone_number) & (Message.receiver_id == users[0].ph_no)) |
+        ((Message.sender_id == users[0].ph_no) & (Message.receiver_id == phone_number))).all()    
+        return render_template('index.html',
+            user_names = users, 
+            receiver_name = users[0].name,  
+            name= Person.query.get(phone_number),
+            messages = messages)
     return render_template('login_signup.html',error = 'Invalid login credentials')
 
 
@@ -64,37 +75,40 @@ def signup():
     phone_number = request.form.get('signupPhoneNumber')
     password = request.form.get('signupPassword')
     confirm_password = request.form.get('confirmPassword')
-
     if password == confirm_password:
-        # Check if the user with the given phone_number already exists
         existing_user = Person.query.filter_by(ph_no=phone_number).first()
-
         if existing_user:
-            # Handle the case where the user already exists
             return render_template('login_signup.html', error='User with this phone number already exists.')
-
-        # Create a new Person instance and add it to the database
         new_user = Person(ph_no=phone_number, name=name, password=password)
         db.session.add(new_user)
         db.session.commit()
-
-        # Redirect to the login/signup page or any other appropriate page
         return render_template('login_signup.html')
-
-    # Handle the case where passwords do not match
     return render_template('login_signup.html', error='Passwords do not match.')
 
 
 
-@app.route('/send_message', methods=['POST'])
-def send_message():
+@app.route('/message_handling', methods=['POST'])
+def message_handling():
+    global users
+    global phn    
     message = request.form.get('message')
-    encrypted = encrypt(message)
-    return render_template('index.html', sended_message = message, received_message = encrypted)
-
-
+    receiver_phno = request.form.get('phone_number')
+    encrypted = encrypt(message) 
+    if message != "" and message != " ":
+        new_message = Message(content=encrypted, sender_id= str(phn) , receiver_id=receiver_phno)
+        db.session.add(new_message)
+        db.session.commit()
+    messages = Message.query.filter(
+    ((Message.sender_id == phn) & (Message.receiver_id == receiver_phno)) |
+    ((Message.sender_id == receiver_phno) & (Message.receiver_id == phn)))
+    return render_template('index.html', 
+        user_names = users, 
+        receiver_name = Person.query.get(receiver_phno).name,
+        name= Person.query.get(phn),
+        messages = messages)
 
 
 if __name__ == '__main__':
     # db.create_all()
+   
     app.run(debug=True)
